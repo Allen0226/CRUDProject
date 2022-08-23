a7fbb7798baa9b53eb5856ca67ff45b8c9c45c39
Allen <rtallen226@gamil.com>
2022-08-23 16:54:58 +0800

    新增資料庫連結

--- CRUDProject/MyService/Program.cs
+++ CRUDProject/MyService/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var NorthDatabaseString = builder.Configuration.GetConnectionString("Northwind");
+builder.Services.AddDbContext<NorthWindContext>(opt =>
+{
+    opt.UseSqlServer(NorthDatabaseString);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
